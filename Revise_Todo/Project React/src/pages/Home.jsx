e9d86059badae9b2a80e0c6ff6460a89808b3f0e
Mohammed-Jameal-J <techprompt1031@gmail.com>
2026-01-16 23:48:40 +0530

    upd

--- Revise_Todo/Project React/src/pages/Home.jsx
+++ Revise_Todo/Project React/src/pages/Home.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <>
+    <div>
     <h4>User Management System</h4>
-    <p>Manage </p>
-    </>
+    <p>Manage your users with full CRUD</p>
+    </div>
   )
 }
 
